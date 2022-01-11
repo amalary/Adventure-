efdbd48052ab2b537a1f221e46e523774775ee82
amalary95 <Malary.Music@gmail.com>
2022-01-11 10:33:08 -0800

    Setting up the logic on JS right now. Just finished the first question

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+let story = 0; 
+let form = document.getElementById('Adventure'); 
+let submit = document.getElementById('continueButton'); 
+let reset = document.getElementById('restButton'); 
+let answers = ''; 
+
+// console.log(form); 
+
+let storyTime = {
+    "start":{
+        "question": "You just arrived to Hogwarts School of magic, which school would you like to join ?",
+        "answers": {
+            "a": "Gryffindor",
+            "b": "Slytherin",
+            "c": "Ravenclaw",
+            "d": "Hufflepuff"
+        }
+    }
+
+}
